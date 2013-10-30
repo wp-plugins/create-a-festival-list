72449c05f1c3c9042d551c644dae2edfc201c896
anoopranawat <anoopranawat@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-30 07:11:33 +0000

    1.1 version
    git-svn-id: https://plugins.svn.wordpress.org/create-a-festival-list/trunk@795730 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ cutomposttype.php
@@ -0,0 +1,348 @@
+<?php
+/*
+Plugin Name: SP Create your custom post
+Plugin URL: http://sptechnolab.com
+Description: A simple your custom post plugin
+Version: 1.1
+Author: SP Technolab
+Author URI: http://sptechnolab.com
+Contributors: SP Technolab
+*/
+/*
+ * Register sp_customposttype
+ *
+ */
+	$option = 'cpt_option';
+	$cptoption = get_option( $option, $default ); 
+	$custom_menuname = $cptoption['menu_name']; 
+		if ($custom_menuname =='')
+			{
+				$custom_menuname = 'Custom Post Name';
+			}
+	$custom_name = $cptoption['post_name']; 
+		if ($custom_name =='')
+			{
+				$custom_name = 'Custom Post Name';
+			}
+	
+	$custom_singular_name = $cptoption['post_singular_name']; 
+			if ($custom_singular_name =='')
+			{
+				$custom_singular_name = 'Custom Post Name';
+			}
+	
+	$custom_shortcode = $cptoption['post_shortcode']; 
+			if ($custom_shortcode =='')
+			{
+				$custom_shortcode = 'YOUR SHORTCODE';
+			}
+	
+function sp_cpt_setup_post_types() {
+
+	global $custom_menuname;
+	global $custom_name;
+	global $custom_singular_name;
+
+	$cpt_labels =  apply_filters( 'sp_festivals_labels', array(
+		'name'                => $custom_name,
+		'singular_name'       => $custom_singular_name,
+		'add_new'             => __('Add New', 'sp_cpt'),
+		'add_new_item'        => __('Add New '.$custom_singular_name, 'sp_cpt'),
+		'edit_item'           => __('Edit '.$custom_singular_name, 'sp_cpt'),
+		'new_item'            => __('New '.$custom_singular_name, 'sp_cpt'),
+		'all_items'           => __('All '.$custom_singular_name, 'sp_cpt'),
+		'view_item'           => __('View ' .$custom_singular_name, 'sp_cpt'),
+		'search_items'        => __('Search '.$custom_singular_name, 'sp_cpt'),
+		'not_found'           => __('No '.$custom_singular_name.' found', 'sp_cpt'),
+		'not_found_in_trash'  => __('No '.$custom_singular_name.' found in Trash', 'sp_cpt'),
+		'parent_item_colon'   => '',
+		'menu_name'           => __($custom_menuname, 'sp_cpt'),
+		'exclude_from_search' => true
+	) );
+
+
+	$cpt_args = array(
+		'labels' 			=> $cpt_labels,
+		'public' 			=> true,
+		'publicly_queryable'=> true,
+		'show_ui' 			=> true,
+		'show_in_menu' 		=> true,
+		'query_var' 		=> true,
+		'capability_type' 	=> 'post',
+		'has_archive' 		=> true,
+		'hierarchical' 		=> false,
+		'supports' => array('title','editor','thumbnail','excerpt'),
+		'taxonomies' => array('category', 'post_tag')
+	);
+	register_post_type( 'sp_cpt', apply_filters( 'sp_cpt_post_type_args', $cpt_args ) );
+
+}
+
+add_action('init', 'sp_cpt_setup_post_types');
+/*
+ * Add [sp_cpt limit="-1"] shortcode
+ *
+ */
+function sp_cpt_shortcode( $atts, $content = null ) {
+	
+	extract(shortcode_atts(array(
+		"limit" => ''
+	), $atts));
+	
+	// Define limit
+	if( $limit ) { 
+		$posts_per_page = $limit; 
+	} else {
+		$posts_per_page = '-1';
+	}
+	
+	ob_start();
+
+	// Create the Query
+	$post_type 		= 'sp_cpt';
+	$orderby 		= 'post_date';
+	$order 			= 'DESC';
+				
+	$query = new WP_Query( array ( 
+								'post_type'      => $post_type,
+								'posts_per_page' => $posts_per_page,
+								'orderby'        => $orderby, 
+								'order'          => $order,
+								'no_found_rows'  => 1
+								) 
+						);
+	
+	//Get post type count
+	$post_count = $query->post_count;
+	$i = 1;
+	
+	// Displays Custom post info
+	if( $post_count > 0) :
+	
+		// Loop
+		while ($query->have_posts()) : $query->the_post();
+		?>
+		<div class="custompost" style="width:100%; float:left; padding-bottom:15px;">
+		
+		<?php if ( has_post_thumbnail() && ! post_password_required() ) : ?>
+		<div class="entry-thumbnail" >
+			<?php the_post_thumbnail(); ?>
+		</div>
+		<?php endif; ?>
+		
+		
+		 <h3><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h3>
+		 
+		
+		 
+		 <div class="entry-meta post-info">
+            <span class="date published time" title="<?php the_time('c') ?>"><?php the_time('F j, Y') ?></span>
+          </div> <!-- .entry-meata .post-info -->
+			<div class="entry-summary">
+		<?php the_excerpt(); ?><a class="moretag" href="<?php the_permalink() ?>"> Read More..</a>
+	</div><!-- .entry-summary -->
+	
+		</div>
+		<?php
+		$i++;
+		endwhile;
+		
+	endif;
+	
+	// Reset query to prevent conflicts
+	wp_reset_query();
+	
+	?>
+	
+	<?php
+	
+	return ob_get_clean();
+
+}
+
+add_shortcode($custom_shortcode, "sp_cpt_shortcode");
+
+class SP_cpt_setting
+{
+    /**
+     * Holds the values to be used in the fields callbacks
+     */
+    private $options;
+
+    /**
+     * Start up
+     */
+    public function __construct()
+    {
+        add_action( 'admin_menu', array( $this, 'add_cpt_page' ) );
+        add_action( 'admin_init', array( $this, 'page_init_cpt' ) );
+    }
+
+    /**
+     * Add options page
+     */
+    public function add_cpt_page()
+    {
+        // This page will be under "Settings"
+        add_options_page(
+            'Settings Admin', 
+            'Custom Post Type Settings', 
+            'manage_options', 
+            'cpt-setting-admin', 
+            array( $this, 'create_cptsadmin_page' )
+        );
+    }
+
+    /**
+     * Options page callback
+     */
+    public function create_cptsadmin_page()
+    {
+        // Set class property
+        $this->options = get_option( 'cpt_option' );
+        ?>
+        <div class="wrap">
+            <?php screen_icon(); ?>
+            <h2>Custom Post Type Settings</h2>           
+            <form method="post" action="options.php">
+            <?php
+                // This prints out all hidden setting fields
+                settings_fields( 'cpt_option_group' );   
+                do_settings_sections( 'cpt-setting-admin' );
+                submit_button(); 
+            ?>
+            </form>
+        </div>
+        <?php
+    }
+
+    /**
+     * Register and add settings
+     */
+    public function page_init_cpt()
+    {        
+        register_setting(
+            'cpt_option_group', // Option group
+            'cpt_option', // Option name
+            array( $this, 'sanitize' ) // Sanitize
+        );
+
+        add_settings_section(
+            'setting_section_id', // ID
+            'Custom Post Type Settings', // Title
+            array( $this, 'print_section_info' ), // Callback
+            'cpt-setting-admin' // Page
+        );  
+
+        add_settings_field(
+            'menu_name', // ID
+            'Menu Name', // Title 
+            array( $this, 'menu_name_callback' ), // Callback
+            'cpt-setting-admin', // Page
+            'setting_section_id' // Section           
+        );      
+
+        add_settings_field(
+            'post_name', 
+            'Name', 
+            array( $this, 'post_name_callback' ), 
+            'cpt-setting-admin', 
+            'setting_section_id'
+        );      
+		add_settings_field(
+            'post_singular_name', // ID
+            'Singular Name ', // Title 
+            array( $this, 'post_singular_name_callback' ), // Callback
+            'cpt-setting-admin', // Page
+            'setting_section_id' // Section           
+        );    
+		add_settings_field(
+            'post_shortcode', // ID
+            'Enter your short code ', // Title 
+            array( $this, 'post_shortcode_callback' ), // Callback
+            'cpt-setting-admin', // Page
+            'setting_section_id' // Section           
+        );    	
+
+       
+    }
+
+    /**
+     * Sanitize each setting field as needed
+     *
+     * @param array $input Contains all settings fields as array keys
+     */
+    public function sanitize( $input )
+    {
+        $new_input = array();
+        if( isset( $input['menu_name'] ) )
+            $new_input['menu_name'] = sanitize_text_field( $input['menu_name'] );
+
+        if( isset( $input['post_name'] ) )
+            $new_input['post_name'] = sanitize_text_field( $input['post_name'] );
+		
+		 if( isset( $input['post_singular_name'] ) )
+            $new_input['post_singular_name'] = sanitize_text_field( $input['post_singular_name'] );
+			
+		 if( isset( $input['post_shortcode'] ) )
+            $new_input['post_shortcode'] = sanitize_text_field( $input['post_shortcode'] );	
+		
+        return $new_input;
+    }
+
+    /** 
+     * Print the Section text
+     */
+    public function print_section_info()
+    {
+        print 'Enter your settings below:';
+    }
+
+    /** 
+     * Get the settings option array and print one of its values
+     */
+    public function menu_name_callback()
+    {
+        printf(
+            '<input type="text" id="menu_name" name="cpt_option[menu_name]" value="%s" />',
+            isset( $this->options['menu_name'] ) ? esc_attr( $this->options['menu_name']) : ''
+        );
+		printf(' For Example <b>Moive</b>');
+    }
+
+    /** 
+     * Get the settings option array and print one of its values
+     */
+	 
+    public function post_name_callback()
+    {
+        printf(
+            '<input type="text" id="post_name" name="cpt_option[post_name]" value="%s" />',
+            isset( $this->options['post_name'] ) ? esc_attr( $this->options['post_name']) : ''
+        );
+			printf(' For Example <b>Moive</b>');
+    }
+	 public function post_singular_name_callback()
+    {
+        printf(
+            '<input type="text" id="post_singular_name" name="cpt_option[post_singular_name]" value="%s" />',
+            isset( $this->options['post_singular_name'] ) ? esc_attr( $this->options['post_singular_name']) : ''
+        );
+		printf(' For Example <b>Moive</b>');
+    }
+	
+	 public function post_shortcode_callback()
+    {
+        printf(
+            '<input type="text" id="post_shortcode" name="cpt_option[post_shortcode]" value="%s" />',
+            isset( $this->options['post_shortcode'] ) ? esc_attr( $this->options['post_shortcode']) : ''
+        );
+		printf(' For Example <b>Moive</b>');
+    }
+
+  
+}
+
+if( is_admin() )
+    $my_cpt_page = new SP_cpt_setting();
+?>
--- readme.txt
+++ readme.txt
@@ -1,23 +1,35 @@
-=== Custom Post type OR Festival List ===
+=== Custom Post type and short code  ===
 Contributors:SP Technolab, anoopranawat 
-Tags: news, custom post type, festival list, section like post, post, page
+Tags: news,  news custom post type, custom post type, shortcode, custom post type plugin festival list, section like post, post, page
 Requires at least: 3.1
-Tested up to: 3.6
+Tested up to: 3.7
 Stable tag: trunk
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-A quick, easy way to add an extensible  custom post type OR  Festival List to Wordpress.
+A quick, easy way to add an extensible custom post type  in your  Wordpress site. 
 
 == Description ==
 
-This plugin add a  custom post type OR  Festival List to your Wordpress site..
+This custom post type plugin add a  custom post  to your Wordpress site. You can use this plugin to create any kind of custom post type.
+  
+The custom post type plugin adds a by default tab name "Custom Post Name"(Which you can change under setting page) to your admin menu, which allows you to enter Custom post Title and Description  items just as you would regular posts.
 
-The plugin adds a Festival tab(you can also change the name if you want to create a custom post type. Just samll change in the code ie 'menu_name'  => __('YOUR CUSTOM POST NAME', 'sp_festivals') to your admin menu, which allows you to enter Festival Title and Description  items just as you would regular posts.
+You can also create your own shortcode to display your custom post.
 
-Now just create a new page and add this short code '[sp_festivals limit="-1"]'.
+Now just create a new page and add this short code <code>[YOUR SHORTCODE limit="-1"]<code> (Please see setting -> Custom Post Type Settings).
+
+= setting -> Custom Post Type Settings =
+* Menu Name  ie Moive
+* Name  ie Moive
+* Singular Name  ie Moive
+* Enter your short code  ie Moive
+<b> So your shortcode will be  <code>[Moive limit="-1"] </code></b>
 
 = Features include: =
+* Create your own Menu tab name
+* Create your own SHORTCODE
+* Setting page
 * Simple plugin so that you can customize it as per your need.
 * With basic wordpress knowladge you can create your own SHORT CODE
 * Easy to configure
@@ -28,32 +40,42 @@ Now just create a new page and add this short code '[sp_festivals limit="-1"]'.
 
 1. Upload the 'Festivals list' folder to the '/wp-content/plugins/' directory.
 2. Activate the Festivals list plugin through the 'Plugins' menu in WordPress.
-3. Add a new page and add this short code "[sp_festivals limit="-1"]".
+3. Add a new page and add this short code <code>[YOUR SHORTCODE limit="-1"]<code>.
+
+= setting -> Custom Post Type Settings =
+* Menu Name  ie Moive
+* Name  ie Moive
+* Singular Name  ie Moive
+* Enter your short code  ie Moive
+<b> So your shortcode will be  <code>[Moive limit="-1"] </code></b>
 
 
 == Frequently Asked Questions ==
 
 = What festivals list templates are available? =
 
-There is one templates named 'festivals.php' which work like same as defult POST TYPE in wordpress.
-
-= What's the easiest way to create my own custom version of the festivals templates? =
-
-Just open "festivals.php" file and change the labels name under FUNCTION sp_festivals_setup_post_types { ... }
+There is one templates named 'cutomposttype.php' which work like same as defult POST TYPE in wordpress.
 
 = Are there shortcodes for festivals items? =
 
-Yes, Add a new page and add this short code '[sp_festivals limit="-1"]'.
+Yes, Add a new page and add this short code <code>[YOUR SHORTCODE limit="-1"]<code>
 
 
 
 == Screenshots ==
 
-1. all Festivals
-2. preview
-3. edit
+1. All Custom Type Posts
+2. Plugin view
+3. Added Menu tab from setting
+4. Seeting page
+5. Single post view
 
 == Changelog ==
+
+= 1.1 =
+* Added setting page
+* Adds custom post type
+* Adds festivals list
 
 = 1.0 =
 * Initial release
@@ -62,6 +84,9 @@ Yes, Add a new page and add this short code '[sp_festivals limit="-1"]'.
 
 
 == Upgrade Notice ==
+
+= 1.1 =
+Added setting page
 
 = 1.0 =
 Initial release
